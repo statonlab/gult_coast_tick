f6ed2a8ca8a9fde9165e29e90ffb9ec8e41d1be1
tlane13 <tlane13utk@gmail.com>
2016-08-09 09:18:41 -0400

    remove false script

--- analysis/5_plinkLD/test_plink.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#/lustre/projects/staton/software/plink_1.9/plink \
-#--vcf sub.vcf --make-bed --out test
-/lustre/projects/staton/software/plink_1.9/plink \
---bfile test --r2 \
---ld-window-r2 0.2
